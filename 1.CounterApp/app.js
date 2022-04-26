2d53404ab27837677d00d21abd28fdd97a69e325
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:38:33 -0500

    Se añadio el primer ejercicio

---
+++ 1.CounterApp/app.js
@@ -0,0 +1,11 @@
+/* Ejercicio 1
+1- Crear funcion para manejar evento clic del boton + con clase 'counter-plus'
+2- Crear funcion para manejar evento clic del boton + con clase 'counter-minus'
+3- Actualizar el div con clase 'counter-display' cada vez que se hace clic en alguno de los botones
+
+Enlaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
+https://developer.mozilla.org/es/docs/Web/API/Element/innerHTML
+*/
+
+// Escribe tu codigo aqui
