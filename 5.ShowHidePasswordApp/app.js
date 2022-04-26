7a780eeef559390c03c9545b57f7060e1ddd2db0
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:41:35 -0500

    Se añadio el quinto ejercicio

---
+++ 5.ShowHidePasswordApp/app.js
@@ -0,0 +1,10 @@
+/* Ejercicio 5
+1- Crear funcion para manejar evento clic del boton con clase 'show-btn',
+esta debera mostrar/ocultar el contenido de la contraseña que se escriba y
+ademas debera cambiar la forma del boton añadiendo y quitando la clase 'hide-btn'
+
+Enlcaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/Element/classList
+*/
+
+// Escribe tu codigo aqui
