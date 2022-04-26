91038d9264e0383582bfb728294e7a8c9e716201
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:40:40 -0500

    Se añadio el cuarto ejercicio

---
+++ 4.StopwatchApp/app.js
@@ -0,0 +1,14 @@
+/* Ejercicio 4
+1- Crear funcion para manejar evento clic del boton start con id 'startTimer'
+2- Crear funcion para simular el comportamiento del cronometro
+--Nota: Teniendo en cuenta que tenemos milisegundos, segundos, minutos y horas,
+  la funcion debe tener en cuenta que si los miliseguntos es igual a 1000 estos deben volver a 0 para iniciar de nuevo e incrementar de 1 en 1 cada vez que los milisegundos llegen a 1000,
+  de igual forma con los segundos si son igual a 60 deben volver a 0 y aumentar los minutos de 1 en 1 y asi con los demas valores.
+3- Crear funcion para manejar evento clic del boton pause con id 'pauseTimer'
+4- Crear funcion para manejar evento clic del boton reset con id 'resetTimer'
+
+Enlcaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/setInterval
+*/
+
+// Escribe tu codigo aqui
