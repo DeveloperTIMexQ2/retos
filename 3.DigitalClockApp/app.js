b45e90c86d1f19812e4401981d33978438927ccd
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:40:13 -0500

    Se añadio el tercer ejercicio

---
+++ 3.DigitalClockApp/app.js
@@ -0,0 +1,9 @@
+/* Ejercicio 3
+1- Crear funcion para mostrar la hora actual y se actualice cada segundo
+
+Enlcaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/setInterval
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
+*/
+
+// Escribe tu codigo aqui
