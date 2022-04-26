4c0e98f1c9ee5d00dd67999c3e28a39912ddf91f
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:42:03 -0500

    Se añadio el sexto ejercicio

---
+++ 6.RandomColorApp/app.js
@@ -0,0 +1,16 @@
+/* Ejercicio 6
+1- Crear funcion para manejar evento clic del boton Generate con id 'gen-btn',
+esta debera generar un string aleatoria con el color en exadecimal, ejemplo #00ff00 y
+ademas debera quitar y añadir la clase 'show-color' para generar una animacion cada vez que se cambia de color,
+el color se debera mostrar en el div con id 'output-color' y el color exadecimal en el input con id 'output'
+2- Opcional - Crear funcion para copiar el color exadecimal al portapapeles
+'''
+Caracteres permitidos en un codigo exadecimal "0123456789abcdef"
+Funcion de ayuda para generar el color: Math.floor(Math.random() * 16)
+'''
+
+Enlcaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/Element/classList
+*/
+
+// Escribe tu codigo aqui
