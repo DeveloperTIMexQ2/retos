38eb32bb6f91d9aae8135020759e056e3bf9c52a
Francisco Amador <developer.ti2@mexq.com.mx>
2022-04-26 12:39:39 -0500

    Se añadio el segundo ejercicio

---
+++ 2.ToDoApp/app.js
@@ -0,0 +1,20 @@
+/* Ejercicio 2
+1- Crear funcion para manejar evento clic del boton add con id 'push'
+2- Validar que e input tenga al menos una letra y si no es asi enviar una alerta
+3- Si el input es valido, añadir la tarea dentro del div con id 'tasks'
+<div class="task">
+  <span id="taskname">
+    ${document.querySelector('#newtask input').value}
+  </span>
+  <button class="delete">
+    <i class="far fa-trash-alt"></i>
+  </button>
+</div>
+4- Crear funcion para manejar el evento clic de los botones de eliminar tarea
+
+Enlaces de ayuda
+https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
+https://developer.mozilla.org/es/docs/Web/API/Element/innerHTML
+*/
+
+// Escribe tu codigo aqui
