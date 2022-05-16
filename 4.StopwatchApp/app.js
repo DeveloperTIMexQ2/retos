7958e06d4bf2c164c8f47d52d6e4ce9a4cf2db78
Claudia Trancoso <angiietrancoso12@gmail.com>
2022-05-16 12:58:43 -0500

    actualizando app.js

--- 4.StopwatchApp/app.js
+++ 4.StopwatchApp/app.js
@@ -12,3 +12,55 @@ https://developer.mozilla.org/es/docs/Web/API/setInterval
 */
 
 // Escribe tu codigo aqui
+'use strict'
+
+var start = document.getElementById('startTimer');
+var reset = document.getElementById('resetTimer');
+var pause = document.getElementById('pauseTimer');
+
+var tiempo = Date.now();
+var acumulador = 0;
+var verificador = false;
+
+initiar();
+
+function initiar() {
+  start.addEventListener('click', startC);
+  pause.addEventListener('click', pauseC);
+  reset.addEventListener('click', resetC);
+}
+
+function startC(){
+  verificador = true;
+}
+
+function pauseC(){
+  verificador = false;
+}
+
+function resetC(){
+  acumulador = 0;
+}
+
+setInterval(() =>{
+  var time = document.querySelector('.timerDisplay');
+  if(verificador){
+    acumulador += Date.now() - tiempo
+  }
+  tiempo = Date.now();
+  time.innerHTML = operaciones(acumulador)
+}, 1000/60);
+
+function operaciones(t){
+  var mili = t % 1000;
+  //contar
+  var cont = Math.floor(((t - mili) / 1000));
+
+  var seg = cont%60;
+  var minu = Math.floor((cont / 60)  % 60);
+  var horas = Math.floor((cont/60 / 60));
+  Number.prototype.c = function(n) {
+    return (this + "").padStart(n,0)
+  }
+  return horas.c(2) + ":" + minu.c(2) + ":" + seg.c(2) + "." + mili.c(3);
+}
