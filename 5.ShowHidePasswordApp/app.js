0993b62077fab90e322814f90b86d73540c4cbac
Claudia Trancoso <angiietrancoso12@gmail.com>
2022-05-16 13:00:13 -0500

    actualizando app.js

--- 5.ShowHidePasswordApp/app.js
+++ 5.ShowHidePasswordApp/app.js
@@ -8,3 +8,22 @@ https://developer.mozilla.org/es/docs/Web/API/Element/classList
 */
 
 // Escribe tu codigo aqui
+'use strict'
+
+var icono = document.getElementById('icon');
+
+console.log(icono);
+
+icono.addEventListener('click', function(){
+    var i = document.querySelector('i');
+
+  if(this.nextElementSibling.type == "password"){
+    this.nextElementSibling.type = "text";
+    i.classList.remove("hide-btn");
+    i.classList.add("fa-eye");
+  }else{
+    this.nextElementSibling.type = "password";
+    i.classList.remove("fa-eye");
+    i.classList.add("hide-btn");
+  }
+});
