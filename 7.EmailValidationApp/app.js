c787ae700eb34642d92c23a53a23c95dbd60189d
Claudia Trancoso <angiietrancoso12@gmail.com>
2022-05-16 13:03:28 -0500

    actualizando app.js

--- 7.EmailValidationApp/app.js
+++ 7.EmailValidationApp/app.js
@@ -12,10 +12,31 @@ https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleRule/style
 let emailId = document.getElementById("email-id");
 let errorMsg = document.getElementById("error-msg");
 let icon = document.getElementById("icon");
+let i1 = document.querySelector(".i1");
+let i2 = document.querySelector(".i2");
 let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/; //Expresion regular a evaluar
 
 function checker() {
   icon.style.display = "inline-block";
   // Escribe tu codigo aqui
+ if(emailId.value.match(mailRegex)){
+    i1.style.display="block";
+    i1.style.color = "#2ecc71"
+    i1.style.border = "2px solid #2ecc71"
+    i2.style.display="none";
+    errorMsg.style.display = "none";
+ }else{
+    i1.style.display="none";
+    i2.style.display="block";
+    i2.style.color = "#ff2851";
+    i2.style.border = "2px solid #ff2851";
+    errorMsg.style.display = "block";
+   }
 
+  if(emailId.value==""){
+    i1.style.display="none";
+    i2.style.display="none";
+    errorMsg.style.display = "none";
+    icon.border = "2px solid #d1d3d4";
+  }
 }
